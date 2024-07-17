0a3016182b8a2a124f9ea66225acd0f9f67a414b
Haha047-UU <1184464111@qq.com>
2024-07-17 16:54:52 +0800

    privaterepo

--- app/api/github.ts
+++ app/api/github.ts
@@ -5,11 +5,14 @@ const octokit = new Octokit({ auth: process.env.access_token });
 export async function getUserRepos(username: string) {  //根据提供的用户名获取用户的仓库列表。
   const response = await octokit.rest.repos.listForUser({
     username,
-    type: 'all',
+    type: 'owner',
     sort: 'updated',
     per_page: 100
   });
-  return response.data;
+
+  const privateRepos = response.data.filter(repo => repo.private);
+
+  return privateRepos;
 }
 
 export async function getRepoDefaultBranch(owner: string, repo: string) {//获取某个仓库的默认分支名称。owner：仓库的所有者，repo：仓库的名称
@@ -40,7 +43,7 @@ function buildTree(items: any[]) {
     const parts = item.path.split('/');
     let current = root;
 
-    parts.forEach((part: string, index: number) => {
+    parts.forEach((index: number) => {
       const path = parts.slice(0, index + 1).join('/');//将 parts 数组中从开始到当前索引（包括当前索引）的子数组连接成一个字符串，用作路径。
       if (!map.has(path)) {
         const newNode = { path, type: item.type, children: [] };
